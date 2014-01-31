1bf70a73525db97378603b2895abf4b1f09bf34e
jtimberman <joshua@opscode.com>
2014-01-30 20:59:01 -0700

    v1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v1.0.2:
+
+- Splunk init script supports status, use it in `stop` action for upgrade.
+
 # v1.0.0:
 
 - Initial release
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@getchef.com'
 license          'Apache 2.0'
 description      'Manage Splunk Enterprise or Splunk Universal Forwarder'
-version          '1.0.1'
+version          '1.0.2'
 
 # for secrets management in setup_auth recipe
 depends 'chef-vault', '>= 1.0.4'
