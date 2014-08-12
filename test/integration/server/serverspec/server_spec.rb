b4b09031476b2bd78565b848db2f77b6443592a4
Ian Garrison <garrison@opscode.com>
2014-08-12 13:51:40 -0700

    added a serverspec test that confirms that when the 'chef-splunk::server' recipe runs that we make sure we're running as the 'splunk' user and that we're listening on port 8443 (need to do this if we're not running as root, priveleged port)

---
+++ test/integration/server/serverspec/server_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'chef-splunk::server should run as "splunk" user' do
+  describe command('ps aux | grep "splunkd -p" | head -1 | awk \'{print $1}\'') do
+    it { should return_stdout 'splunk' }
+  end
+
+  #describe file('/opt/splunk/var/log') do
+  #  it { should be_owned_by 'splunk' }
+  #end
+end
+
+describe 'chef-splunk::server should listen on web_port 8443' do
+  describe port(8443) do
+    it { should be_listening.with('tcp') }
+  end
+end
