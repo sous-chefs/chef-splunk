5580276a68614087ef6c8fcfdf8d090cea197476
Ian Garrison <garrison@opscode.com>
2014-08-25 17:04:34 -0700

    made some further changes to support switching from root to splunk and vice versa

--- recipes/service.rb
+++ recipes/service.rb
@@ -18,10 +18,15 @@
 # limitations under the License.
 #
 
-if node['splunk']['is_server'] && !node['splunk']['server']['runasroot'] && !File.exists?(splunk_dir)
+myuser = 'root'
+unless node['splunk']['server']['runasroot']
+  myuser = node['splunk']['user']['username']
+end
+
+if node['splunk']['is_server']
   directory splunk_dir do
-    owner node['splunk']['user']['username']
-    group node['splunk']['user']['username']
+    owner myuser
+    group myuser
     mode 00755
   end
 
@@ -50,23 +55,19 @@ if node['splunk']['accept_license']
   end
 end
 
-def chown_R_splunk(triggerfile)
+def chown_R_splunk(triggerfile, user)
   if node['splunk']['is_server']
     ruby_block "splunk_fix_file_ownership" do
       block do
-        FileUtils.chown_R(node['splunk']['user']['username'], node['splunk']['user']['username'], splunk_dir)
+        FileUtils.chown_R(user, user, splunk_dir)
       end
       only_if { ::File.stat(triggerfile).uid.eql?(0) }
-      not_if { node['splunk']['server']['runasroot'] }
     end
   end
 end
 
-chown_R_splunk("#{splunk_dir}/etc/users")
-postinsttriggerfile = "#{splunk_dir}/etc/.setup_admin_password"
-if File.exists? postinsttriggerfile
-  chown_R_splunk(postinsttriggerfile)
-end
+chown_R_splunk("#{splunk_dir}/etc/users", myuser)
+chown_R_splunk(splunk_dir, myuser)
 
 template "/etc/init.d/splunk" do
   source "splunk-init.erb"
