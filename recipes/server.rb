8972df1c6f33a258daf8f5648b4e4ed79355cc07
Ryan LeViseur <vidkun@users.noreply.github.com>
2016-08-16 15:50:51 -0500

    include setup_shclustering recipe in the server recipe if enabled

--- recipes/server.rb
+++ recipes/server.rb
@@ -48,3 +48,7 @@ end
 if node['splunk']['clustering']['enabled']
   include_recipe 'chef-splunk::setup_clustering'
 end
+
+if node['splunk']['shclustering']['enabled']
+  include_recipe 'chef-splunk::setup_shclustering'
+end
