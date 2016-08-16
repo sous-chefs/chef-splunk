da10cbf3f82faa32da77d60e2780aacef6b90cca
Ryan LeViseur <vidkun@users.noreply.github.com>
2016-08-16 08:42:32 -0500

    add a bare recipe to setup shclustering to get initial specs to pass

---
+++ recipes/setup_shclustering.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: splunk
+# Recipe:: setup_shclustering
+#
+# Author: Ryan LeViseur <ryanlev@gmail.com>
+# Copyright (c) 2014, Chef Software, Inc <legal@chef.io>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+unless node['splunk']['shclustering']['enabled']
+  Chef::Log.debug('The chef-splunk::setup_shclustering recipe was added to the node,')
+  Chef::Log.debug('but the attribute to enable search head clustering was not set.')
+  return
+end
+
+# ensure that the splunk service resource is available without cloning
+# the resource (CHEF-3694). this is so the later notification works,
+# especially when using chefspec to run this cookbook's specs.
+begin
+  resources('service[splunk]')
+rescue Chef::Exceptions::ResourceNotFound
+  service 'splunk'
+end
