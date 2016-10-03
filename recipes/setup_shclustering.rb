93e47828f92c2e8fad304ccfd223b02881d3c50b
Ryan LeViseur <vidkun@users.noreply.github.com>
2016-10-03 07:56:03 -0500

    convert intermixed tabs to space. nice catch @rarsan

--- recipes/setup_shclustering.rb
+++ recipes/setup_shclustering.rb
@@ -42,53 +42,53 @@ shcluster_params = node['splunk']['shclustering']
 # unless shcluster members are staticly assigned via the node attribute,
 # try to find the other shcluster members via Chef search
 if shcluster_params['shcluster_members'].empty?
-	shcluster_servers_list = Array.new
-	search( # ~FC003
-	  :node,
-	  "\
-	  splunk_shclustering_enabled:true AND \
-	  splunk_shclustering_label:#{node['splunk']['shclustering']['label']} AND \
-	  chef_environment:#{node.chef_environment}"
-	).each do |result|
-		shcluster_servers_list << result['splunk']['shclustering']['mgmt_uri']
-	end
+  shcluster_servers_list = Array.new
+  search( # ~FC003
+    :node,
+    "\
+    splunk_shclustering_enabled:true AND \
+    splunk_shclustering_label:#{node['splunk']['shclustering']['label']} AND \
+    chef_environment:#{node.chef_environment}"
+  ).each do |result|
+    shcluster_servers_list << result['splunk']['shclustering']['mgmt_uri']
+  end
 else
-	shcluster_servers_list = shcluster_params['shcluster_members'].join(',')
+  shcluster_servers_list = shcluster_params['shcluster_members'].join(',')
 end
 
 # create app directories to house our server.conf with our shcluster configuration
 shcluster_app_dir = "#{splunk_dir}/etc/apps/0_autogen_shcluster_config"
 
 directory "#{shcluster_app_dir}" do
-	owner node['splunk']['user']['username']
+  owner node['splunk']['user']['username']
   group node['splunk']['user']['username']
   mode 0755
 end
 
 directory "#{shcluster_app_dir}/local" do
-	owner node['splunk']['user']['username']
+  owner node['splunk']['user']['username']
   group node['splunk']['user']['username']
   mode 0755
 end
 
 template "#{shcluster_app_dir}/local/server.conf" do
-	source "shclustering/server.conf.erb"
-	mode 0755
-	variables(
+  source "shclustering/server.conf.erb"
+  mode 0755
+  variables(
     :shcluster_params => node['splunk']['shclustering'],
     :shcluster_secret => shcluster_secret
   )
   sensitive true
-	notifies :restart, 'service[splunk]', :immediately
+  notifies :restart, 'service[splunk]', :immediately
 end
 
 # bootstrap the shcluster and elect a captain if initial_captain set to true and this is the initial shcluster build
 if node['splunk']['shclustering']['mode'] == "captain"
-	execute 'bootstrap-shcluster' do
-	  command "#{splunk_cmd} bootstrap shcluster-captain -servers_list '#{shcluster_servers_list.join(',')}' -auth '#{splunk_auth_info}'"
-	  not_if { ::File.exist?("#{splunk_dir}/etc/.setup_shcluster") }
-	  notifies :restart, 'service[splunk]'
-	end
+  execute 'bootstrap-shcluster' do
+    command "#{splunk_cmd} bootstrap shcluster-captain -servers_list '#{shcluster_servers_list.join(',')}' -auth '#{splunk_auth_info}'"
+    not_if { ::File.exist?("#{splunk_dir}/etc/.setup_shcluster") }
+    notifies :restart, 'service[splunk]'
+  end
 end
 
 file "#{splunk_dir}/etc/.setup_shcluster" do
