55bbec7e7926ffb8e1440086931fc0c4c00d93c6
Juan Carlos Castillo Cano <juancarlos_castillocano@intuit.com>
2016-12-02 12:57:35 +0000

    Right set of tests for client.rb
    Signed-off-by: Juan Carlos Castillo Cano <jccastillocano@gmail.com>

--- spec/recipes/client_spec.rb
+++ spec/recipes/client_spec.rb
@@ -5,7 +5,7 @@ describe 'chef-splunk::client' do
     ChefSpec::ServerRunner.new.converge(described_recipe)
   end
 
-  it 'includes user, install_forwarder and setup recipes' do
+  it 'includes user, install_forwarder and setup recipes (setup is performing client config)' do
     expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('chef-splunk::user')
     expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('chef-splunk::install_forwarder')
     expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('chef-splunk::setup')
