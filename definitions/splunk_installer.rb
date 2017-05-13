acc2b920794a628b473451e907729977ba301760
rarsan <roy.arsan@gmail.com>
2017-05-13 15:34:24 -0700

    Add back support to SUSE platform - tested on openSUSE Leap 42.2

--- definitions/splunk_installer.rb
+++ definitions/splunk_installer.rb
@@ -51,10 +51,10 @@ define :splunk_installer, url: nil do
   end
 
   local_package_resource = case node['platform_family']
-                           when 'rhel', 'fedora'  then :rpm_package
-                           when 'debian'          then :dpkg_package
-                           when 'omnios'          then :solaris_package
-                           end
+                  when 'rhel', 'fedora', 'suse'  then :rpm_package
+                  when 'debian'                  then :dpkg_package
+                  when 'omnios'                  then :solaris_package
+                  end
 
   declare_resource local_package_resource, params[:name] do
     source cached_package.gsub(/\.Z/, '')
