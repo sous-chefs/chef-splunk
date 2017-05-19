1f9bb3c80ab694a58b1731687e70a842ba725a0b
rarsan <roy.arsan@gmail.com>
2017-05-18 22:47:09 -0500

    Add global config for ChefSpec Runner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,7 @@ RSpec.configure do |config|
   config.color = true               # Use color in STDOUT
   config.formatter = :documentation # Use the specified formatter
   config.log_level = :error         # Avoid deprecation notice SPAM
+
+  config.platform = 'ubuntu'
+  config.version = '16.04'
 end
