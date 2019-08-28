3248a4dee489a56072dcfb3799392bbde6fbeec2
Tim Smith <tsmith84@gmail.com>
2019-08-27 21:00:18 -0700

    Remove long_description per cookstyle
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Manage Splunk Enterprise or Splunk Universal Forwarder'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.7.3'
 
 supports 'debian'
