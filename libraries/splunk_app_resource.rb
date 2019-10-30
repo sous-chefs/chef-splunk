327ea3354bff96cb65b16b6c916cabb7e1669524
Tim Smith <tsmith84@gmail.com>
2019-10-30 14:10:02 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/splunk_app_resource.rb
+++ libraries/splunk_app_resource.rb
@@ -23,7 +23,6 @@ class Chef
       self.resource_name = 'splunk_app'
 
       # Actions correspond to splunk commands pertaining to apps.
-      actions :enable, :disable, :install, :remove
       default_action :enable
       state_attrs :enabled, :installed
 
