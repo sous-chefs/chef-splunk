sous-chefs/chef-splunk

commit 26fa04d965b059bf15ff7dbe98a3fd40834b3eed

- Installs Enterprise Splunk 8.0.1 - Installs Splunk Universal Forwarder 8.0.1 - Removes unnecessary calls to `include_recipe 'chef-vault'` - in `chef-splunk::client`, do not install the forwarder if the server config is being installed   on a splunkd server - adds helper method: `#server?` that will return true if the chef node is a splunkd server - adds helper method: `#port_open?` that will return true if the local node has a specified port open - logs a warning message if `node['splunk']['upgrade']['server_url']` or `node['splunk']['upgrade']['forwarder_url']` exist - Splunk now generates its own scripts for boot starts; therefore this cookbook executes Splunk's `boot-start`   command
Signed-off-by: Dang H. Nguyen <dang.nguyen@disney.com>

kitchen.dokken.yml
@@ -10,6 +10,16 @@ transport:
 provisioner:
   name: dokken
   deprecations_as_errors: true
+  attributes:
+    dev_mode: true
+    splunk:
+      accept_license: true
+      enable_ssl: false
+      ssl_options:
+        enableSplunkWebSSL: 0
+        httpport: 8000
+        startwebserver: 1
+      web_port: 8000
 
 verifier:
   root_path: '/opt/verifier'
@@ -187,8 +197,6 @@ suites:
           runasroot: true
         is_server: true
         accept_license: true
-        ssl_options:
-          enable_ssl: true
 
   - name: server-runas-splunk
     run_list:
@@ -200,9 +208,7 @@ suites:
           runasroot: false
         is_server: true
         accept_license: true
-        ssl_options:
-          enable_ssl: true
-        web_port: 8443
+        web_port: 8000
 
   - name: server-cluster-master
     run_list:
@@ -219,8 +225,6 @@ suites:
           mode: master
           replication_factor: 5
           search_factor: 3
-        ssl_options:
-          enable_ssl: true
 
   - name: server-shcluster-member
     run_list:
@@ -234,9 +238,7 @@ suites:
         accept_license: true
         shclustering:
           enabled: true
-        ssl_options:
-          enable_ssl: true
-        web_port: 8443
+        web_port: 8000
 
   - name: disabled
     run_list:

recipes/service.rb
@@ -2,7 +2,7 @@
 # Cookbook:: chef-splunk
 # Recipe:: service
 #
-# Copyright:: 2014-2016, Chef Software, Inc.
+# Copyright:: 2014-2019, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ unless license_accepted?
   raise 'Splunk license was not accepted'
 end
 
-if node['splunk']['is_server']
+if server?
   directory splunk_dir do
     owner splunk_runas_user
     group splunk_runas_user
@@ -47,14 +47,6 @@ if node['splunk']['is_server']
   end
 end
 
-# ftr = first time run file created by a splunk install
-execute 'splunk enable boot-start' do
-  user 'root'
-  command "#{splunk_cmd} enable boot-start --answer-yes --no-prompt#{license_accepted? ? ' --accept-license' : ''}"
-  only_if { ::File.exist? "#{splunk_dir}/ftr" }
-  notifies :create, 'template[/etc/init.d/splunk]'
-end
-
 # If we run as splunk user do a recursive chown to that user for all splunk
 # files if a few specific files are root owned.
 ruby_block 'splunk_fix_file_ownership' do
@@ -68,42 +60,15 @@ end
 
 Chef::Log.info("Node init package: #{node['init_package']}")
 
-template '/etc/systemd/system/splunkd.service' do
-  source 'splunk-systemd.erb'
-  mode '644'
-  variables(
-    splunkdir: splunk_dir,
-    splunkcmd: splunk_cmd,
-    runasroot: node['splunk']['server']['runasroot'] == true,
-    accept_license: license_accepted?
-  )
-  notifies :run, 'execute[systemctl daemon-reload]', :immediately
-  only_if { node['init_package'] == 'systemd' }
-end
-
 execute 'systemctl daemon-reload' do
   action :nothing
   only_if { node['init_package'] == 'systemd' }
 end
 
-template '/etc/init.d/splunk' do
-  source 'splunk-init.erb'
-  mode '700'
-  variables(
-    splunkdir: splunk_dir,
-    splunkuser: splunk_runas_user,
-    splunkcmd: splunk_cmd,
-    runasroot: node['splunk']['server']['runasroot'] == true,
-    accept_license: license_accepted?
-  )
-  notifies :run, 'execute[systemctl daemon-reload]', :immediately
-  notifies :restart, 'service[splunk]'
-end
-
 # during an initial install, the start/restart commands must deal with accepting
 # the license. So, we must ensure the service[splunk] resource
-# properly deals with the license.
-edit_resource(:service, 'splunk') do
+# properly deals with the license by way of the `svc_command` helper.
+service 'splunk' do
   action node['init_package'] == 'systemd' ? %i(start enable) : :start
   supports status: true, restart: true
   stop_command svc_command('stop')

templates/splunk-init.erb
@@ -1,74 +1,0 @@
-#!/bin/sh
-#
-# /etc/init.d/splunk
-# init script for Splunk.
-# generated by 'splunk enable boot-start'.
-#
-### BEGIN INIT INFO
-# Provides: splunkd
-# Required-Start: $remote_fs
-# Required-Stop: $remote_fs
-# Default-Start: 2 3 4 5
-# Default-Stop: 0 1 6
-# Short-Description: Start splunk
-# Description: Splunk indexer service
-### END INIT INFO
-#
-RETVAL=0
-
-splunk_start() {
-  echo Starting Splunk...
-<% if @runasroot %>
-  <%= @splunkcmd %> start --answer-yes --no-prompt
-<% else %>
-  su - <%= @splunkuser %> -c '<%= @splunkcmd %> start --answer-yes --no-prompt'
-<% end %>
-  RETVAL=$?
-}
-splunk_stop() {
-  echo Stopping Splunk...
-<% if @runasroot %>
-  <%= @splunkcmd %> stop
-<% else %>
-  su - <%= @splunkuser %> -c '<%= @splunkcmd %> stop'
-<% end %>
-  RETVAL=$?
-}
-splunk_restart() {
-  echo Restarting Splunk...
-<% if @runasroot %>
-  <%= @splunkcmd %> restart
-<% else %>
-  su - <%= @splunkuser %> -c '<%= @splunkcmd %> restart'
-<% end %>
-  RETVAL=$?
-}
-splunk_status() {
-  echo Splunk status:
-<% if @runasroot %>
-  <%= @splunkcmd %> status
-<% else %>
-  su - <%= @splunkuser %> -c '<%= @splunkcmd %> status'
-<% end %>
-  RETVAL=$?
-}
-case "$1" in
-  start)
-    splunk_start
-    ;;
-  stop)
-    splunk_stop
-    ;;
-  restart)
-    splunk_restart
-    ;;
-  status)
-    splunk_status
-    ;;
-  *)
-    echo "Usage: $0 {start|stop|restart|status}"
-    exit 1
-    ;;
-esac
-
-exit $RETVAL

templates/splunk-systemd.erb
@@ -1,22 +1,0 @@
-# systemd service file for splunk
-
-[Unit]
-Description=Splunk
-After=network.target
-
-[Service]
-Type=forking
-RemainAfterExit=no
-<% if ! @runasroot %>
-User=splunk
-Group=splunk
-<% end %>
-LimitNOFILE=65536
-ExecStart=<%= @splunkcmd %> start --answer-yes --no-prompt
-ExecStop=<%= @splunkcmd %> stop
-ExecReload=<%= @splunkcmd %> restart
-PIDFile=<%= @splunkdir %>/var/run/splunk/splunkd.pid
-
-[Install]
-WantedBy=multi-user.target
-Alias=splunk.service

attributes/default.rb
@@ -24,6 +24,7 @@ default['splunk']['mgmt_port']      = '8089'
 default['splunk']['web_port']       = '443'
 default['splunk']['ratelimit_kilobytessec'] = '2048'
 default['splunk']['disabled'] = false
+default['splunk']['data_bag'] = 'vault'
 
 default['splunk']['setup_auth'] = true
 default['splunk']['user'] = {
@@ -91,17 +92,17 @@ default['splunk']['splunk_servers'] = []
 
 default['splunk']['forwarder'] = {
   'url' => value_for_platform_family(
-    %w(rhel fedora suse amazon) => 'https://download.splunk.com/products/universalforwarder/releases/6.6.0/linux/splunkforwarder-6.6.0-1c4f3bbe1aea-linux-2.6-x86_64.rpm',
-    ['debian'] => 'https://download.splunk.com/products/universalforwarder/releases/6.6.0/linux/splunkforwarder-6.6.0-1c4f3bbe1aea-linux-2.6-amd64.deb'
+    %w(rhel fedora suse) => 'https://download.splunk.com/products/universalforwarder/releases/8.0.1/linux/splunkforwarder-8.0.1-6db836e2fb9e-linux-2.6-x86_64.rpm',
+    'debian' => 'https://download.splunk.com/products/universalforwarder/releases/8.0.1/linux/splunkforwarder-8.0.1-6db836e2fb9e-linux-2.6-amd64.deb'
   ),
-  'version' => '6.6.0',
+  'version' => '8.0.1',
 }
 
 default['splunk']['server'] = {
   'runasroot' => true,
   'url' => value_for_platform_family(
-    %w(rhel fedora suse amazon) => 'https://download.splunk.com/products/splunk/releases/6.6.0/linux/splunk-6.6.0-1c4f3bbe1aea-linux-2.6-x86_64.rpm',
-    ['debian'] => 'https://download.splunk.com/products/splunk/releases/6.6.0/linux/splunk-6.6.0-1c4f3bbe1aea-linux-2.6-amd64.deb'
+    %w(redhat fedora suse) => 'https://download.splunk.com/products/splunk/releases/8.0.1/linux/splunk-8.0.1-6db836e2fb9e-linux-2.6-x86_64.rpm',
+    'debian' => 'https://download.splunk.com/products/splunk/releases/8.0.1/linux/splunk-8.0.1-6db836e2fb9e-linux-2.6-amd64.deb'
   ),
-  'version' => '6.6.0',
+  'version' => '8.0.1',
 }

attributes/upgrade.rb
@@ -17,26 +17,23 @@
 # limitations under the License.
 
 # assumes x86_64 only (is there any reason to test i386 anymore?)
-# the `server_url` and `forwarder_url` is deprecated, but kept here for
-# backwards-compatibility
-default['splunk']['upgrade']['server_url'] = value_for_platform_family(
-  %w(rhel fedora amazon) => 'https://download.splunk.com/products/splunk/releases/7.3.2/linux/splunk-7.3.2-c60db69f8e32-linux-2.6-x86_64.rpm',
-  ['debian'] => 'https://download.splunk.com/products/splunk/releases/7.3.2/linux/splunk-7.3.2-c60db69f8e32-linux-2.6-amd64.deb'
+default['splunk']['forwarder']['upgrade']['version'] = '8.0.1'
+default['splunk']['server']['upgrade']['version'] = '8.0.1'
+
+default['splunk']['server']['upgrade']['url'] = value_for_platform(
+  %w(redhat fedora suse centos amazon) => {
+    'default' => 'https://download.splunk.com/products/splunk/releases/8.0.1/linux/splunk-8.0.1-6db836e2fb9e-linux-2.6-x86_64.rpm',
+  },
+  %w(debian ubuntu) => {
+    'default' => 'https://download.splunk.com/products/splunk/releases/8.0.1/linux/splunk-8.0.1-6db836e2fb9e-linux-2.6-amd64.deb',
+  }
 )
 
-default['splunk']['upgrade']['forwarder_url'] = value_for_platform_family(
-  %w(rhel fedora amazon) => 'https://download.splunk.com/products/universalforwarder/releases/7.3.2/linux/splunkforwarder-7.3.2-c60db69f8e32-linux-2.6-x86_64.rpm',
-  ['debian'] => 'https://download.splunk.com/products/universalforwarder/releases/7.3.2/linux/splunkforwarder-7.3.2-c60db69f8e32-linux-2.6-amd64.deb'
+default['splunk']['forwarder']['upgrade']['url'] = value_for_platform(
+  %w(redhat fedora suse centos amazon) => {
+    'default' => 'https://download.splunk.com/products/universalforwarder/releases/8.0.1/linux/splunkforwarder-8.0.1-6db836e2fb9e-linux-2.6-x86_64.rpm',
+  },
+  %w(debian ubuntu) => {
+    'default' => 'https://download.splunk.com/products/universalforwarder/releases/8.0.1/linux/splunkforwarder-8.0.1-6db836e2fb9e-linux-2.6-amd64.deb',
+  }
 )
-
-# as of v3.0.1, this structure was introduced to stay in similar structure to
-# node['splunk']['server'] when specifying URL and version attributes
-default['splunk']['server']['upgrade'] = {
-  'url' => node['splunk']['upgrade']['server_url'],
-  'version' => '7.3.2',
-}
-
-default['splunk']['forwarder']['upgrade'] = {
-  'url' => node['splunk']['upgrade']['forwarder_url'],
-  'version' => '7.3.2',
-}

kitchen.yml
@@ -10,6 +10,12 @@ provisioner:
     dev_mode: true
     splunk:
       accept_license: true
+      enable_ssl: false
+      ssl_options:
+        enableSplunkWebSSL: 0
+        httpport: 8000
+        startwebserver: 1
+      web_port: 80
 
 platforms:
   - name: centos-6
@@ -76,8 +82,8 @@ suites:
           runasroot: false
         is_server: true
         ssl_options:
-          enable_ssl: true
-        web_port: 8443
+          enable_ssl: false
+        web_port: 80
 
   - name: server-cluster-master
     run_list:
@@ -106,8 +112,8 @@ suites:
         shclustering:
           enabled: true
         ssl_options:
-          enable_ssl: true
-        web_port: 8443
+          enable_ssl: false
+        web_port: 80
 
   - name: disabled
     run_list:

libraries/helpers.rb
@@ -17,6 +17,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+require 'socket'
+require 'mixlib/shellout'
+
 def splunk_installed?
   ::File.exist?(splunk_cmd)
 end
@@ -52,13 +55,9 @@ def splunk_dir
   # (intermediary) server which installs to /opt/splunkforwarder
   forwarderpath = '/opt/splunkforwarder'
   enterprisepath = '/opt/splunk'
-  if node['splunk']['is_intermediate'] == true
-    forwarderpath
-  elsif node['splunk']['is_server'] == true
-    enterprisepath
-  else
-    forwarderpath
-  end
+
+  return enterprisepath if server?
+  forwarderpath
 end
 
 def splunk_auth(auth)
@@ -93,4 +92,49 @@ end
 
 def license_accepted?
   node['splunk']['accept_license'] == true
+end
+
+# a splunkd instance is either a splunk client (runs universal forwarder only) or a complete server
+def server?
+  node['splunk']['is_server'] == true
+end
+
+def port_open?(port, ip = '127.0.0.1')
+  # TCPSocket will return a file descriptor if it can open the connection,
+  # and raise Errno::ECONNREFUSED or Errno::ETIMEDOUT if it can't. We rescue
+  # that exception and return false.
+  begin
+    ::TCPSocket.new(ip, port)
+  rescue Errno::ECONNREFUSED, Errno::ETIMEDOUT
+    return false
+  end
+  true
+end
+
+def current_mgmt_port
+  splunk = Mixlib::ShellOut.new("#{splunk_cmd} show splunkd-port | awk -F: '{print$NF}'")
+  splunk.run_command
+  splunk.error! # Raise an exception if it didn't exit with 0
+  splunk.stdout.strip
+end
+
+def disabled?
+  node['splunk'].attribute?('disabled') &&
+    node['splunk']['disabled'] == true
+end
+
+def setup_auth?
+  node['splunk']['setup_auth'] == true
+end
+
+def enable_ssl?
+  node['splunk']['ssl_options']['enable_ssl'] == true
+end
+
+def enable_clustering?
+  node['splunk']['clustering']['enabled'] == true
+end
+
+def enable_shclustering?
+  node['splunk']['shclustering']['enabled'] == true
 end

commit 1a1fbe96349a57b4c135c2814745e9ecd80f695a

Fixes a regression made by commit 26fa04d9: when `node['splunk']['runasroot']` is false, splunk isn't started with a non-root user or the startup scripts are not modified to allow for non-root splunk commands
Signed-off-by: Dang H. Nguyen <dang.nguyen@disney.com>

kitchen.dokken.yml
@@ -94,20 +94,6 @@ platforms:
         OPTIMISTIC_ABOUT_FILE_LOCKING=1\n' > /opt/splunk/etc/splunk-launch.conf
     pid_one_command: /sbin/init
 
-- name: fedora-latest
-  driver:
-    image: dokken/fedora-latest
-    pid_one_command: /usr/lib/systemd/systemd
-    intermediate_instructions:
-      - RUN yum install chkconfig -y
-      - RUN mkdir -p /data/splunk-test
-      # Disable file locking check by Splunk as it fails on unsupported file systems used in some Docker hosts (e.g. on Mac)
-      - RUN mkdir -p /opt/splunk/etc
-      - RUN printf '
-        SPLUNK_SERVER_NAME=Splunkd\n
-        SLPUNK_WEB_NAME=splunkweb\n
-        OPTIMISTIC_ABOUT_FILE_LOCKING=1\n' > /opt/splunk/etc/splunk-launch.conf
-
 - name: ubuntu-16.04
   driver:
     image: dokken/ubuntu-16.04
@@ -129,18 +115,6 @@ platforms:
     intermediate_instructions:
       - RUN /usr/bin/apt-get -y update
       - RUN mkdir -p /data/splunk-test
-      # Disable file locking check by Splunk as it fails on unsupported file systems used in some Docker hosts (e.g. on Mac)
-      - RUN mkdir -p /opt/splunk/etc
-      - RUN printf '
-        SPLUNK_SERVER_NAME=Splunkd\n
-        SLPUNK_WEB_NAME=splunkweb\n
-        OPTIMISTIC_ABOUT_FILE_LOCKING=1\n' > /opt/splunk/etc/splunk-launch.conf
-
-- name: opensuse-leap
-  driver:
-    image: dokken/opensuse-leap
-    pid_one_command: /bin/systemd
-    intermediate_instructions:
       # Disable file locking check by Splunk as it fails on unsupported file systems used in some Docker hosts (e.g. on Mac)
       - RUN mkdir -p /opt/splunk/etc
       - RUN printf '

recipes/service.rb
@@ -59,9 +59,37 @@ end
 
 Chef::Log.info("Node init package: #{node['init_package']}")
 
+template '/etc/systemd/system/splunkd.service' do
+  source 'splunk-systemd.erb'
+  mode '644'
+  variables(
+    splunkdir: splunk_dir,
+    splunkcmd: splunk_cmd,
+    runasroot: false,
+    accept_license: license_accepted?
+  )
+  notifies :run, 'execute[systemctl daemon-reload]', :immediately
+  only_if { node['init_package'] == 'systemd' && node['splunk']['server']['runasroot'] == false }
+end
+
 execute 'systemctl daemon-reload' do
   action :nothing
   only_if { node['init_package'] == 'systemd' }
+end
+
+template '/etc/init.d/splunk' do
+  source 'splunk-init.erb'
+  mode '700'
+  variables(
+    splunkdir: splunk_dir,
+    splunkuser: splunk_runas_user,
+    splunkcmd: splunk_cmd,
+    runasroot: false,
+    accept_license: license_accepted?
+  )
+  not_if { node['splunk']['server']['runasroot'] == true }
+  notifies :run, 'execute[systemctl daemon-reload]', :immediately
+  notifies :restart, 'service[splunk]'
 end
 
 # during an initial install, the start/restart commands must deal with accepting

templates/splunk-init.erb
@@ -1,0 +1,77 @@
+#!/bin/sh
+#
+# /etc/init.d/splunk
+# init script for Splunk.
+# generated by Chef Infra Client.
+#
+### BEGIN INIT INFO
+# Provides: splunkd
+# Required-Start: $remote_fs
+# Required-Stop: $remote_fs
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Start splunk
+# Description: Splunk indexer service
+### END INIT INFO
+#
+RETVAL=0
+
+splunk_start() {
+  echo Starting Splunk...
+<% if @runasroot %>
+  "<%= @splunkcmd %>" start --answer-yes --no-prompt
+<% else %>
+  su - <%= @splunkuser %> -c '"<%= @splunkcmd %>" start --answer-yes --no-prompt'
+<% end %>
+  RETVAL=$?
+  [ $RETVAL -eq 0 ] && touch /var/lock/subsys/splunk
+}
+splunk_stop() {
+  echo Stopping Splunk...
+<% if @runasroot %>
+  "<%= @splunkcmd %>" stop
+<% else %>
+  su - <%= @splunkuser %> -c '"<%= @splunkcmd %>" stop'
+<% end %>
+  RETVAL=$?
+  [ $RETVAL -eq 0 ] && rm -f /var/lock/subsys/splunk
+}
+splunk_restart() {
+  echo Restarting Splunk...
+<% if @runasroot %>
+  "<%= @splunkcmd %>" restart
+<% else %>
+  su - <%= @splunkuser %> -c '"<%= @splunkcmd %>" restart'
+<% end %>
+  RETVAL=$?
+  [ $RETVAL -eq 0 ] && touch /var/lock/subsys/splunk
+}
+splunk_status() {
+  echo Splunk status:
+<% if @runasroot %>
+  "<%= @splunkcmd %>" status
+<% else %>
+  su - <%= @splunkuser %> -c '"<%= @splunkcmd %>" status'
+<% end %>
+  RETVAL=$?
+}
+case "$1" in
+  start)
+    splunk_start
+    ;;
+  stop)
+    splunk_stop
+    ;;
+  restart)
+    splunk_restart
+    ;;
+  status)
+    splunk_status
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart|status}"
+    exit 1
+    ;;
+esac
+
+exit $RETVAL

templates/splunk-systemd.erb
@@ -1,0 +1,22 @@
+# systemd service file for splunk
+
+[Unit]
+Description=Splunk
+After=network.target
+
+[Service]
+Type=forking
+RemainAfterExit=no
+<% if ! @runasroot %>
+User=splunk
+Group=splunk
+<% end %>
+LimitNOFILE=65536
+ExecStart="<%= @splunkcmd %>" start --answer-yes --no-prompt
+ExecStop="<%= @splunkcmd %>" stop
+ExecReload="<%= @splunkcmd %>" restart
+PIDFile=<%= @splunkdir %>/var/run/splunk/splunkd.pid
+
+[Install]
+WantedBy=multi-user.target
+Alias=splunk.service