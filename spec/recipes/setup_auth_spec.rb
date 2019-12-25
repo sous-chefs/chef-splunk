sous-chefs/chef-splunk

commit 26fa04d965b059bf15ff7dbe98a3fd40834b3eed

- Installs Enterprise Splunk 8.0.1 - Installs Splunk Universal Forwarder 8.0.1 - Removes unnecessary calls to `include_recipe 'chef-vault'` - in `chef-splunk::client`, do not install the forwarder if the server config is being installed   on a splunkd server - adds helper method: `#server?` that will return true if the chef node is a splunkd server - adds helper method: `#port_open?` that will return true if the local node has a specified port open - logs a warning message if `node['splunk']['upgrade']['server_url']` or `node['splunk']['upgrade']['forwarder_url']` exist - Splunk now generates its own scripts for boot starts; therefore this cookbook executes Splunk's `boot-start`   command
Signed-off-by: Dang H. Nguyen <dang.nguyen@disney.com>

attributes/default.rb
@@ -24,6 +24,7 @@ default['splunk']['mgmt_port']      = '8089'
 default['splunk']['web_port']       = '443'
 default['splunk']['ratelimit_kilobytessec'] = '2048'
 default['splunk']['disabled'] = false
+default['splunk']['data_bag'] = 'vault'
 
 default['splunk']['setup_auth'] = true
 default['splunk']['user'] = {
@@ -91,17 +92,17 @@ default['splunk']['splunk_servers'] = []
 
 default['splunk']['forwarder'] = {
   'url' => value_for_platform_family(
-    %w(rhel fedora suse amazon) => 'https://download.splunk.com/products/universalforwarder/releases/6.6.0/linux/splunkforwarder-6.6.0-1c4f3bbe1aea-linux-2.6-x86_64.rpm',
-    ['debian'] => 'https://download.splunk.com/products/universalforwarder/releases/6.6.0/linux/splunkforwarder-6.6.0-1c4f3bbe1aea-linux-2.6-amd64.deb'
+    %w(rhel fedora suse) => 'https://download.splunk.com/products/universalforwarder/releases/8.0.1/linux/splunkforwarder-8.0.1-6db836e2fb9e-linux-2.6-x86_64.rpm',
+    'debian' => 'https://download.splunk.com/products/universalforwarder/releases/8.0.1/linux/splunkforwarder-8.0.1-6db836e2fb9e-linux-2.6-amd64.deb'
   ),
-  'version' => '6.6.0',
+  'version' => '8.0.1',
 }
 
 default['splunk']['server'] = {
   'runasroot' => true,
   'url' => value_for_platform_family(
-    %w(rhel fedora suse amazon) => 'https://download.splunk.com/products/splunk/releases/6.6.0/linux/splunk-6.6.0-1c4f3bbe1aea-linux-2.6-x86_64.rpm',
-    ['debian'] => 'https://download.splunk.com/products/splunk/releases/6.6.0/linux/splunk-6.6.0-1c4f3bbe1aea-linux-2.6-amd64.deb'
+    %w(redhat fedora suse) => 'https://download.splunk.com/products/splunk/releases/8.0.1/linux/splunk-8.0.1-6db836e2fb9e-linux-2.6-x86_64.rpm',
+    'debian' => 'https://download.splunk.com/products/splunk/releases/8.0.1/linux/splunk-8.0.1-6db836e2fb9e-linux-2.6-amd64.deb'
   ),
-  'version' => '6.6.0',
+  'version' => '8.0.1',
 }

kitchen.dokken.yml
@@ -10,6 +10,16 @@ transport:
 provisioner:
   name: dokken
   deprecations_as_errors: true
+  attributes:
+    dev_mode: true
+    splunk:
+      accept_license: true
+      enable_ssl: false
+      ssl_options:
+        enableSplunkWebSSL: 0
+        httpport: 8000
+        startwebserver: 1
+      web_port: 8000
 
 verifier:
   root_path: '/opt/verifier'
@@ -187,8 +197,6 @@ suites:
           runasroot: true
         is_server: true
         accept_license: true
-        ssl_options:
-          enable_ssl: true
 
   - name: server-runas-splunk
     run_list:
@@ -200,9 +208,7 @@ suites:
           runasroot: false
         is_server: true
         accept_license: true
-        ssl_options:
-          enable_ssl: true
-        web_port: 8443
+        web_port: 8000
 
   - name: server-cluster-master
     run_list:
@@ -219,8 +225,6 @@ suites:
           mode: master
           replication_factor: 5
           search_factor: 3
-        ssl_options:
-          enable_ssl: true
 
   - name: server-shcluster-member
     run_list:
@@ -234,9 +238,7 @@ suites:
         accept_license: true
         shclustering:
           enabled: true
-        ssl_options:
-          enable_ssl: true
-        web_port: 8443
+        web_port: 8000
 
   - name: disabled
     run_list:

commit 28c016f0952811cbe4a24a3c6536ca86ec65c039

Deployer provisioning and bugfixes (#132)
* Modifies the `chef-splunk::shclustering` to deploy a Splunk Search Head deployer

* Fixes a regression made by commit 26fa04d9: when `node['splunk']['runasroot']` is false, splunk isn't started with a non-root user or the startup scripts are not modified to allow for non-root splunk commands

* add pattern `kitchen.*` to chefignore file

* adds an execute resource to stop the splunk daemon properly if the daemon needs to be running as a non-root user

* updates the change log

* adds `status_command` for the service[splunk] resources; needed due to the accept-license requirement of splunk

* sets the splunk user home directory to the appropriate path when `node['splunk']['is_server']` is true

* ensures the splunk service resource starts the daemon

* ensures the splunk daemon is always running as the correct user; restarts as needed

* updates chefspecs to pass recent changes

* removing spec directory from .gitignore to fix shared examples

Signed-off-by: Dang H. Nguyen <dang.nguyen@disney.com>

.gitignore
@@ -53,8 +53,6 @@ examples/*
 Procfile
 .kitchen*
 .rubocop.yml
-spec/*
-.travis.yml
 .foodcritic
 appveyor.yml
 
@@ -73,7 +71,6 @@ appveyor.yml
 
 # Berkshelf #
 #############
-Berksfile
 Berksfile.lock
 cookbooks/*
 tmp

.travis.yml
@@ -60,9 +60,9 @@ matrix:
     - before_script:
       - chef --version
       - cookstyle --version
-      - foodcritic --version
     - script:
-      - delivery local all
+      - delivery local lint
+      - delivery local unit
       env:
         - UNIT_AND_LINT=1
         - CHEF_LICENSE=accept

attributes/default.rb
@@ -59,7 +59,7 @@ default['splunk']['clustering'] = {
 
 default['splunk']['shclustering'] = {
   'enabled' => false,
-  'mode' => 'member', # member|captain
+  'mode' => 'member', # member|captain|deployer
   'label' => 'shcluster1',
   'replication_factor' => 3,
   'replication_port' => 9900,

chefignore
@@ -55,6 +55,7 @@ examples/*
 Guardfile
 Procfile
 .kitchen*
+kitchen.*
 .rubocop.yml
 spec/*
 Rakefile

kitchen.dokken.yml
@@ -94,20 +94,6 @@ platforms:
         OPTIMISTIC_ABOUT_FILE_LOCKING=1\n' > /opt/splunk/etc/splunk-launch.conf
     pid_one_command: /sbin/init
 
-- name: fedora-latest
-  driver:
-    image: dokken/fedora-latest
-    pid_one_command: /usr/lib/systemd/systemd
-    intermediate_instructions:
-      - RUN yum install chkconfig -y
-      - RUN mkdir -p /data/splunk-test
-      # Disable file locking check by Splunk as it fails on unsupported file systems used in some Docker hosts (e.g. on Mac)
-      - RUN mkdir -p /opt/splunk/etc
-      - RUN printf '
-        SPLUNK_SERVER_NAME=Splunkd\n
-        SLPUNK_WEB_NAME=splunkweb\n
-        OPTIMISTIC_ABOUT_FILE_LOCKING=1\n' > /opt/splunk/etc/splunk-launch.conf
-
 - name: ubuntu-16.04
   driver:
     image: dokken/ubuntu-16.04
@@ -129,18 +115,6 @@ platforms:
     intermediate_instructions:
       - RUN /usr/bin/apt-get -y update
       - RUN mkdir -p /data/splunk-test
-      # Disable file locking check by Splunk as it fails on unsupported file systems used in some Docker hosts (e.g. on Mac)
-      - RUN mkdir -p /opt/splunk/etc
-      - RUN printf '
-        SPLUNK_SERVER_NAME=Splunkd\n
-        SLPUNK_WEB_NAME=splunkweb\n
-        OPTIMISTIC_ABOUT_FILE_LOCKING=1\n' > /opt/splunk/etc/splunk-launch.conf
-
-- name: opensuse-leap
-  driver:
-    image: dokken/opensuse-leap
-    pid_one_command: /bin/systemd
-    intermediate_instructions:
       # Disable file locking check by Splunk as it fails on unsupported file systems used in some Docker hosts (e.g. on Mac)
       - RUN mkdir -p /opt/splunk/etc
       - RUN printf '
@@ -225,6 +199,21 @@ suites:
           mode: master
           replication_factor: 5
           search_factor: 3
+
+  - name: server-shdeployer
+    run_list:
+      - recipe[chef-splunk]
+    attributes:
+      dev_mode: true
+      splunk:
+        server:
+          runasroot: false
+        is_server: true
+        accept_license: true
+        shclustering:
+          enabled: true
+          mode: deployer
+        web_port: 8000
 
   - name: server-shcluster-member
     run_list:

kitchen.yml
@@ -101,6 +101,21 @@ suites:
         ssl_options:
           enable_ssl: true
 
+  - name: server-shdeployer
+    run_list:
+      - recipe[chef-splunk]
+    attributes:
+      splunk:
+        server:
+          runasroot: false
+        is_server: true
+        shclustering:
+          enabled: true
+          mode: deployer
+        ssl_options:
+          enable_ssl: false
+        web_port: 80
+
   - name: server-shcluster-member
     run_list:
       - recipe[chef-splunk]

libraries/helpers.rb
@@ -138,3 +138,10 @@ end
 def enable_shclustering?
   node['splunk']['shclustering']['enabled'] == true
 end
+
+# returns true if the splunkd process is owned by the correct "run-as" user
+def correct_runas_user?
+  splunk = Mixlib::ShellOut.new("ps -ef|grep splunk|grep -v grep|awk '{print$1}'|uniq")
+  splunk.run_command
+  splunk_runas_user == splunk.stdout
+end

metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Manage Splunk Enterprise or Splunk Universal Forwarder'
-version '4.0.1'
+version '4.0.2'
 
 supports 'debian', '>= 8.9'
 supports 'ubuntu', '>= 16.04'

recipes/client.rb
@@ -48,7 +48,17 @@ edit_resource(:service, 'splunk') do
   stop_command svc_command('stop')
   start_command svc_command('start')
   restart_command svc_command('restart')
+  status_command svc_command('status')
+  notifies :run, "execute[#{splunk_cmd} stop]", :before unless correct_runas_user?
   provider splunk_service_provider
+end
+
+# if the splunk daemon is running as root, executing a normal service restart or stop will fail if the boot
+# start script has been modified to execute splunk as a non-root user.
+# So, the splunk daemon must be run this way instead
+execute "#{splunk_cmd} stop" do
+  action :nothing
+  not_if { node['splunk']['server']['runasroot'] == true }
 end
 
 directory "#{splunk_dir}/etc/system/local" do

recipes/server.rb
@@ -16,7 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-require 'socket'
 node.default['splunk']['is_server'] = true
 include_recipe 'chef-splunk::user'
 include_recipe 'chef-splunk::install_server'
@@ -32,6 +31,7 @@ edit_resource(:service, 'splunk') do
   stop_command svc_command('stop')
   start_command svc_command('start')
   restart_command svc_command('restart')
+  status_command svc_command('status')
   provider splunk_service_provider
 end
 
@@ -46,10 +46,15 @@ execute 'update-splunk-mgmt-port' do
   notifies :restart, 'service[splunk]'
 end
 
-execute 'enable-splunk-receiver-port' do
-  command "#{splunk_cmd} enable listen #{node['splunk']['receiver_port']} -auth '#{splunk_auth_info}'"
+ruby_block 'enable-splunk-receiver-port' do
   sensitive true
+  block do
+    splunk = Mixlib::ShellOut.new("#{splunk_cmd} enable listen #{node['splunk']['receiver_port']} -auth #{splunk_auth_info}")
+    splunk.run_command
+    true if splunk.stderr.include?("Configuration for port #{node['splunk']['receiver_port']} already exists")
+  end
   not_if { port_open?(node['splunk']['receiver_port']) }
+  notifies :restart, 'service[splunk]'
 end
 
 include_recipe 'chef-splunk::setup_ssl' if enable_ssl?

recipes/service.rb
@@ -59,9 +59,38 @@ end
 
 Chef::Log.info("Node init package: #{node['init_package']}")
 
+template '/etc/systemd/system/splunkd.service' do
+  source 'splunk-systemd.erb'
+  mode '644'
+  variables(
+    splunkdir: splunk_dir,
+    splunkcmd: splunk_cmd,
+    runasroot: false,
+    accept_license: license_accepted?
+  )
+  notifies :run, 'execute[systemctl daemon-reload]', :immediately
+  only_if { node['init_package'] == 'systemd' && node['splunk']['server']['runasroot'] == false }
+end
+
 execute 'systemctl daemon-reload' do
   action :nothing
   only_if { node['init_package'] == 'systemd' }
+end
+
+template '/etc/init.d/splunk' do
+  source 'splunk-init.erb'
+  mode '700'
+  variables(
+    splunkdir: splunk_dir,
+    splunkuser: splunk_runas_user,
+    splunkcmd: splunk_cmd,
+    runasroot: false,
+    accept_license: license_accepted?
+  )
+  not_if { node['splunk']['server']['runasroot'] == true }
+  notifies :run, 'execute[systemctl daemon-reload]', :immediately
+  notifies :run, "execute[#{splunk_cmd} stop]", :immediately # must use this if splunk daemon is running as root and needs to switch to non-root user
+  notifies :restart, 'service[splunk]'
 end
 
 # during an initial install, the start/restart commands must deal with accepting
@@ -73,5 +102,15 @@ service 'splunk' do
   stop_command svc_command('stop')
   start_command svc_command('start')
   restart_command svc_command('restart')
+  status_command svc_command('status')
+  notifies :run, "execute[#{splunk_cmd} stop]", :before unless correct_runas_user?
   provider splunk_service_provider
+end
+
+# if the splunk daemon is running as root, executing a normal service restart or stop will fail if the boot
+# start script has been modified to execute splunk as a non-root user.
+# So, the splunk daemon must be run this way instead
+execute "#{splunk_cmd} stop" do
+  action :nothing
+  not_if { node['splunk']['server']['runasroot'] == true }
 end

recipes/setup_auth.rb
@@ -37,6 +37,7 @@ edit_resource(:service, 'splunk') do
   stop_command svc_command('stop')
   start_command svc_command('start')
   restart_command svc_command('restart')
+  status_command svc_command('status')
   provider splunk_service_provider
 end
 

recipes/setup_clustering.rb
@@ -26,11 +26,12 @@ end
 # the license. So, we must ensure the service[splunk] resource
 # properly deals with the license.
 edit_resource(:service, 'splunk') do
-  action :nothing
+  action node['init_package'] == 'systemd' ? %i(start enable) : :start
   supports status: true, restart: true
   stop_command svc_command('stop')
   start_command svc_command('start')
   restart_command svc_command('restart')
+  status_command svc_command('status')
   provider splunk_service_provider
 end
 

recipes/setup_shclustering.rb
@@ -27,11 +27,12 @@ end
 # the license. So, we must ensure the service[splunk] resource
 # properly deals with the license; hence, the use of `#svc_command` method calls here.
 edit_resource(:service, 'splunk') do
-  action :nothing
+  action node['init_package'] == 'systemd' ? %i(start enable) : :start
   supports status: true, restart: true
   stop_command svc_command('stop')
   start_command svc_command('start')
   restart_command svc_command('restart')
+  status_command svc_command('status')
   provider splunk_service_provider
 end
 
@@ -67,8 +68,27 @@ template "#{shcluster_app_dir}/local/server.conf" do
   notifies :restart, 'service[splunk]', :immediately
 end
 
+# finish here if the server is the Splunk Search Head deployer; otherwise, continue on
+return if node['splunk']['shclustering']['mode'] == 'deployer'
+
 # bootstrap the shcluster and the node as a captain if shclustering mode is set to 'captain'
 shcluster_servers_list = node['splunk']['shclustering']['shcluster_members']
+
+# search for the fqdn of the search head deployer and set that as the deployer_url
+# if one is not given in the node attributes
+if node['splunk']['shclustering']['deployer_url'].empty?
+  search(
+    :node,
+    "\
+    splunk_shclustering_enabled:true AND \
+    splunk_shclustering_label:#{node['splunk']['shclustering']['label']} AND \
+    splunk_shclustering_mode:deployer AND \
+    chef_environment:#{node.chef_environment}",
+    filter_result: { 'deployer_fqdn' => ['fqdn'] }
+  ).each do |fqdn|
+    node.default['splunk']['shclustering']['deployer_url'] = fqdn
+  end
+end
 
 # unless shcluster members are staticly assigned via the node attribute,
 # try to find the other shcluster members via Chef search

recipes/setup_ssl.rb
@@ -33,11 +33,12 @@ certs = chef_vault_item(
 # the license. So, we must ensure the service[splunk] resource
 # properly deals with the license.
 edit_resource(:service, 'splunk') do
-  action :nothing
+  action node['init_package'] == 'systemd' ? %i(start enable) : :start
   supports status: true, restart: true
   stop_command svc_command('stop')
   start_command svc_command('start')
   restart_command svc_command('restart')
+  status_command svc_command('status')
   provider splunk_service_provider
 end
 

recipes/user.rb
@@ -2,7 +2,7 @@
 # Cookbook:: chef-splunk
 # Recipe:: user
 #
-# Copyright:: 2014-2016, Chef Software, Inc.
+# Copyright:: 2014-2020, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,6 +22,8 @@ group node['splunk']['user']['username'] do
   system true if %w(linux).include?(node['os'])
 end
 
+node.default['splunk']['user']['home'] = splunk_dir
+
 user node['splunk']['user']['username'] do
   comment node['splunk']['user']['comment']
   shell node['splunk']['user']['shell']
@@ -29,4 +31,5 @@ user node['splunk']['user']['username'] do
   uid node['splunk']['user']['uid']
   home node['splunk']['user']['home']
   system true if %w(linux).include?(node['os'])
+  notifies :stop, 'service[splunk]', :before if splunk_installed?
 end

templates/shclustering/server.conf.erb
@@ -1,15 +1,17 @@
-# Dropped off by Chef
-
-[replication_port://<%= @shcluster_params['replication_port'] %>]
+# Managed by Chef. Changes will be reverted
 
 [shclustering]
-disabled = 0
-mgmt_uri = <%= @shcluster_params['mgmt_uri'] %>
-replication_factor = <%= @shcluster_params['replication_factor'] %>
 shcluster_label = <%= @shcluster_params['label'] %>
-<% unless @shcluster_params['deployer_url'] == '' -%>
-conf_deploy_fetch_url = <%= @shcluster_params['deployer_url'] %>
-<% end -%>
 <% unless @shcluster_secret.nil? -%>
 pass4SymmKey = <%= @shcluster_secret %>
 <% end -%>
+<% if %w(member captain).include? @shcluster_params['mode'] %>
+disabled = 0
+mgmt_uri = <%= @shcluster_params['mgmt_uri'] %>
+replication_factor = <%= @shcluster_params['replication_factor'] %>
+<% unless @shcluster_params['deployer_url'] == '' -%>
+conf_deploy_fetch_url = <%= @shcluster_params['deployer_url'] %>
+<% end -%>
+
+[replication_port://<%= @shcluster_params['replication_port'] %>]
+<% end %>

templates/splunk-init.erb
@@ -1,0 +1,77 @@
+#!/bin/sh
+#
+# /etc/init.d/splunk
+# init script for Splunk.
+# generated by Chef Infra Client.
+#
+### BEGIN INIT INFO
+# Provides: splunkd
+# Required-Start: $remote_fs
+# Required-Stop: $remote_fs
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Start splunk
+# Description: Splunk indexer service
+### END INIT INFO
+#
+RETVAL=0
+
+splunk_start() {
+  echo Starting Splunk...
+<% if @runasroot %>
+  "<%= @splunkcmd %>" start --answer-yes --no-prompt
+<% else %>
+  su - <%= @splunkuser %> -c '"<%= @splunkcmd %>" start --answer-yes --no-prompt'
+<% end %>
+  RETVAL=$?
+  [ $RETVAL -eq 0 ] && touch /var/lock/subsys/splunk
+}
+splunk_stop() {
+  echo Stopping Splunk...
+<% if @runasroot %>
+  "<%= @splunkcmd %>" stop
+<% else %>
+  su - <%= @splunkuser %> -c '"<%= @splunkcmd %>" stop'
+<% end %>
+  RETVAL=$?
+  [ $RETVAL -eq 0 ] && rm -f /var/lock/subsys/splunk
+}
+splunk_restart() {
+  echo Restarting Splunk...
+<% if @runasroot %>
+  "<%= @splunkcmd %>" restart
+<% else %>
+  su - <%= @splunkuser %> -c '"<%= @splunkcmd %>" restart'
+<% end %>
+  RETVAL=$?
+  [ $RETVAL -eq 0 ] && touch /var/lock/subsys/splunk
+}
+splunk_status() {
+  echo Splunk status:
+<% if @runasroot %>
+  "<%= @splunkcmd %>" status
+<% else %>
+  su - <%= @splunkuser %> -c '"<%= @splunkcmd %>" status'
+<% end %>
+  RETVAL=$?
+}
+case "$1" in
+  start)
+    splunk_start
+    ;;
+  stop)
+    splunk_stop
+    ;;
+  restart)
+    splunk_restart
+    ;;
+  status)
+    splunk_status
+    ;;
+  *)
+    echo "Usage: $0 {start|stop|restart|status}"
+    exit 1
+    ;;
+esac
+
+exit $RETVAL

templates/splunk-systemd.erb
@@ -1,0 +1,22 @@
+# systemd service file for splunk
+
+[Unit]
+Description=Splunk
+After=network.target
+
+[Service]
+Type=forking
+RemainAfterExit=no
+<% if ! @runasroot %>
+User=splunk
+Group=splunk
+<% end %>
+LimitNOFILE=65536
+ExecStart="<%= @splunkcmd %>" start --answer-yes --no-prompt
+ExecStop="<%= @splunkcmd %>" stop
+ExecReload="<%= @splunkcmd %>" restart
+PIDFile=<%= @splunkdir %>/var/run/splunk/splunkd.pid
+
+[Install]
+WantedBy=multi-user.target
+Alias=splunk.service

spec/recipes/server_spec.rb
@@ -24,10 +24,7 @@ describe 'chef-splunk::server' do
     end
 
     it 'enables receiver port' do
-      expect(chef_run).to run_execute('enable-splunk-receiver-port').with(
-        command: "/opt/splunk/bin/splunk enable listen 9997 -auth 'admin:notarealpassword'",
-        sensitive: true
-      )
+      expect(chef_run).to run_ruby_block('enable-splunk-receiver-port').with(sensitive: true)
     end
   end
 
@@ -37,6 +34,10 @@ describe 'chef-splunk::server' do
       chef_run_init.node.force_default['splunk']['accept_license'] = true
       chef_run_init.node.force_default['splunk']['mgmt_port'] = '9089'
       chef_run_init.converge(described_recipe)
+    end
+
+    before do
+      allow_any_instance_of(Chef::Resource).to receive(:port_open?).and_return(false)
     end
 
     it 'updates splunkd management port' do
@@ -52,10 +53,7 @@ describe 'chef-splunk::server' do
     end
 
     it 'enables receiver port' do
-      expect(chef_run).to run_execute('enable-splunk-receiver-port').with(
-        command: "/opt/splunk/bin/splunk enable listen 9997 -auth 'admin:notarealpassword'",
-        sensitive: true
-      )
+      expect(chef_run).to run_ruby_block('enable-splunk-receiver-port').with(sensitive: true)
     end
   end
 end

spec/recipes/setup_auth_spec.rb
@@ -1,0 +1,34 @@
+require 'spec_helper'
+
+describe 'chef-splunk::setup_auth' do
+  context 'default attributes' do
+    let(:chef_run) do
+      ChefSpec::ServerRunner.new do |node, _server|
+        node.force_default['dev_mode'] = true
+        node.force_default['splunk']['is_server'] = true
+        node.force_default['splunk']['accept_license'] = true
+      end
+    end
+
+    before do
+      allow_any_instance_of(Chef::Recipe).to receive(:chef_vault_item).and_return('auth' => 'admin:notarealpassword')
+    end
+
+    context 'setup_auth is true' do
+      it 'created user-seed.conf and notifies splunk restart' do
+        chef_run.converge(described_recipe)
+        expect(chef_run).to create_template('user-seed.conf')
+          .with(mode: '600', sensitive: true, owner: 'root', group: 'root')
+        expect(chef_run.template('user-seed.conf')).to notify('service[splunk]').to(:restart).immediately
+      end
+    end
+
+    context 'setup auth is false' do
+      it 'logs debug message' do
+        chef_run.node.force_default['splunk']['setup_auth'] = false
+        chef_run.converge(described_recipe)
+        expect(chef_run).to write_log('setup_auth is disabled').with(level: :debug)
+      end
+    end
+  end
+end

spec/recipes/setup_shclustering_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe 'chef-splunk::setup_shclustering' do
+  let(:splunk_local_dir) { '/opt/splunk/etc/apps/0_autogen_shcluster_config/local' }
+  let(:server_conf_file) { "#{splunk_local_dir}/server.conf" }
+
+  let(:vault_item) do
+    { 'auth' => 'admin:notarealpassword', 'secret' => 'notarealsecret' }
+  end
+
   let(:deployer_node) do
     stub_node(platform: 'ubuntu', version: '16.04') do |node|
       node.automatic['fqdn'] = 'deploy.cluster.example.com'
@@ -10,6 +17,24 @@ describe 'chef-splunk::setup_shclustering' do
       node.force_default['splunk']['shclustering']['enabled'] = true
       node.force_default['splunk']['accept_license'] = true
     end
+  end
+
+  context 'search head deployer' do
+    let(:chef_run) do
+      ChefSpec::ServerRunner.new do |node|
+        node.force_default['dev_mode'] = true
+        node.force_default['splunk']['is_server'] = true
+        node.force_default['splunk']['shclustering']['enabled'] = true
+        node.force_default['splunk']['accept_license'] = true
+        node.force_default['splunk']['shclustering']['mode'] = 'deployer'
+      end.converge(described_recipe)
+    end
+
+    before do
+      allow_any_instance_of(Chef::Recipe).to receive(:chef_vault_item).and_return(vault_item)
+    end
+
+    it_behaves_like 'common server.conf settings'
   end
 
   context 'search head cluster member settings' do
@@ -29,51 +54,34 @@ describe 'chef-splunk::setup_shclustering' do
       end.converge(described_recipe)
     end
 
-    let(:vault_item) do
-      { 'auth' => 'admin:notarealpassword', 'secret' => 'notarealsecret' }
-    end
-
     before do
       allow_any_instance_of(Chef::Recipe).to receive(:chef_vault_item).and_return(vault_item)
     end
+
+    it_behaves_like 'common server.conf settings'
 
     it 'writes a file marker to ensure convergence' do
       expect(chef_run).to render_file('/opt/splunk/etc/.setup_shcluster').with_content("true\n")
     end
 
     it 'writes server.conf with replication port' do
-      expect(chef_run).to render_file('/opt/splunk/etc/apps/0_autogen_shcluster_config/local/server.conf')
+      expect(chef_run).to render_file(server_conf_file)
         .with_content("[replication_port://#{chef_run.node['splunk']['shclustering']['replication_port']}]")
     end
 
-    it 'writes server.conf with a shclustering stanza' do
-      expect(chef_run).to render_file('/opt/splunk/etc/apps/0_autogen_shcluster_config/local/server.conf')
-        .with_content('[shclustering]')
-    end
-
     it 'writes server.conf with the deployer url' do
-      expect(chef_run).to render_file('/opt/splunk/etc/apps/0_autogen_shcluster_config/local/server.conf')
+      expect(chef_run).to render_file(server_conf_file)
         .with_content("conf_deploy_fetch_url = #{chef_run.node['splunk']['shclustering']['deployer_url']}")
     end
 
     it 'writes server.conf with the node mgmt uri' do
-      expect(chef_run).to render_file('/opt/splunk/etc/apps/0_autogen_shcluster_config/local/server.conf')
+      expect(chef_run).to render_file(server_conf_file)
         .with_content("mgmt_uri = #{chef_run.node['splunk']['shclustering']['mgmt_uri']}")
     end
 
     it 'writes server.conf with the shcluster replication factor' do
-      expect(chef_run).to render_file('/opt/splunk/etc/apps/0_autogen_shcluster_config/local/server.conf')
+      expect(chef_run).to render_file(server_conf_file)
         .with_content("replication_factor = #{chef_run.node['splunk']['shclustering']['replication_factor']}")
-    end
-
-    it 'writes server.conf with the shcluster label' do
-      expect(chef_run).to render_file('/opt/splunk/etc/apps/0_autogen_shcluster_config/local/server.conf')
-        .with_content("shcluster_label = #{chef_run.node['splunk']['shclustering']['label']}")
-    end
-
-    it 'writes server.conf with the shcluster secret' do
-      expect(chef_run).to render_file('/opt/splunk/etc/apps/0_autogen_shcluster_config/local/server.conf')
-        .with_content('pass4SymmKey = notarealsecret')
     end
 
     it 'does not run command to bootstrap captain' do

spec/recipes/setup_ssl_spec.rb
@@ -29,9 +29,7 @@ describe 'chef-splunk::setup_ssl' do
         allow_any_instance_of(Chef::Recipe).to receive(:chef_vault_item).and_return(vault_item)
       end
 
-      it 'created the service[splunk] resource' do
-        expect(chef_run.service('splunk')).to do_nothing
-      end
+      it_behaves_like 'splunk daemon'
 
       it 'writes web.conf with http port 443' do
         expect(chef_run).to render_file('/opt/splunk/etc/system/local/web.conf').with_content('httpport = 443')
@@ -78,9 +76,7 @@ describe 'chef-splunk::setup_ssl' do
         allow_any_instance_of(Chef::Recipe).to receive(:chef_vault_item).and_return(vault_item)
       end
 
-      it 'created the service[splunk] resource' do
-        expect(chef_run.service('splunk')).to do_nothing
-      end
+      it_behaves_like 'splunk daemon'
 
       it 'writes web.conf with a non-default port for https' do
         expect(chef_run).to render_file('/opt/splunk/etc/system/local/web.conf').with_content('httpport = 7777')

spec/shared_examples.rb
@@ -1,0 +1,26 @@
+shared_examples 'common server.conf settings' do
+  let(:splunk_local_dir) { '/opt/splunk/etc/apps/0_autogen_shcluster_config/local' }
+  let(:server_conf_file) { "#{splunk_local_dir}/server.conf" }
+
+  it 'writes server.conf with a shclustering stanza' do
+    expect(chef_run).to render_file(server_conf_file)
+      .with_content('[shclustering]')
+  end
+
+  it 'writes server.conf with the shcluster label' do
+    expect(chef_run).to render_file(server_conf_file)
+      .with_content("shcluster_label = #{chef_run.node['splunk']['shclustering']['label']}")
+  end
+
+  it 'writes server.conf with the shcluster secret' do
+    expect(chef_run).to render_file(server_conf_file)
+      .with_content('pass4SymmKey = notarealsecret')
+  end
+end
+
+shared_examples 'splunk daemon' do
+  it 'created the service[splunk] resource' do
+    expect(chef_run).to start_service('splunk')
+    expect(chef_run).to enable_service('splunk')
+  end
+end

spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+require 'shared_examples'
 
 RSpec.configure do |config|
   config.color = true               # Use color in STDOUT