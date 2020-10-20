d3138844df22c7c4c91a369de47942823209124a
Dang H. Nguyen <dang.nguyen@disney.com>
2020-10-19 19:56:08 -0700

    bumped cookbook version to 6.4.0
    Signed-off-by: Dang H. Nguyen <dang.nguyen@disney.com>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Manage Splunk Enterprise or Splunk Universal Forwarder'
-version '6.3.0'
+version '6.4.0'
 
 supports 'debian', '>= 8.9'
 supports 'ubuntu', '>= 16.04'
